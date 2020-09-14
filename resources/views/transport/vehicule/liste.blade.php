a0b8286b569cc5f0bddc514e817bb90644ebeb42
Izz24 <r.thur.light@gmail.com>
2020-09-14 20:10:22 +0000

    Maincourante commit

--- resources/views/transport/vehicule/liste.blade.php
+++ resources/views/transport/vehicule/liste.blade.php
@@ -1,40 +1,45 @@
 @extends('base')
 
 @section('main')
-<div class="row">
-    <div class="col-sm-12">
-        <table class="table table-bordered table-hover" style="width: 100%" id="liste">
-            <thead>
-            <tr>
-                <td>Immatriculation</td>
-                <td>Marque</td>
-                <td>Type</td>
-                <td>Code</td>
-                <td>N°Chassis</td>
-                <td>DPMC</td>
-                <td>Date acquisition</td>
-                <td>centre</td>
-                <td>Centre Régional</td>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($vehicules as $vehicule)
-            <tr>
-                <td>{{$vehicule->immatriculation}}</td>
-                <td>{{$vehicule->marque}}</td>
-                <td>{{$vehicule->type}}</td>
-                <td>{{$vehicule->code}}</td>
-                <td>{{$vehicule->num_chassis}}</td>
-                <td>{{$vehicule->DPMC}}</td>
-                <td>{{$vehicule->dateAcquisition}}</td>
-                <td>{{$vehicule->centre}}</td>
-                <td>{{$vehicule->centreRegional}}</td>
-            </tr>
-            @endforeach
-            </tbody>
-        </table>
+<div class="burval-container">
+    <div><h2 class="heading">Site</h2></div>
+    <br />
+    <div class="row">
+        <div class="col-sm-12">
+            <table class="table table-bordered table-hover" style="width: 100%" id="liste">
+                <thead>
+                <tr>
+                    <td>Immatriculation</td>
+                    <td>Marque</td>
+                    <td>Type</td>
+                    <td>Code</td>
+                    <td>N°Chassis</td>
+                    <td>DPMC</td>
+                    <td>Date acquisition</td>
+                    <td>centre</td>
+                    <td>Centre Régional</td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($vehicules as $vehicule)
+                <tr>
+                    <td>{{$vehicule->immatriculation}}</td>
+                    <td>{{$vehicule->marque}}</td>
+                    <td>{{$vehicule->type}}</td>
+                    <td>{{$vehicule->code}}</td>
+                    <td>{{$vehicule->num_chassis}}</td>
+                    <td>{{$vehicule->DPMC}}</td>
+                    <td>{{$vehicule->dateAcquisition}}</td>
+                    <td>{{$vehicule->centre}}</td>
+                    <td>{{$vehicule->centreRegional}}</td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
+
 <script>
     $(document).ready(function () {
         $('#liste').DataTable({
