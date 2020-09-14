4d83942267d6eeb7a1b6037e5d9b4663a541bb0f
Arthur <r.thur.light@gmail.com>
2020-09-14 07:45:27 +0000

    Third commit

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,22 +3,22 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BURVAL</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('magnificpopup/magnific-popup.css') }}">
-    <link href="{{ asset('css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/burval.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('magnificpopup/magnific-popup.css') }}">
+    <link href="{{ secure_asset('css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/burval.css') }}" rel="stylesheet">
 
-    <script src="{{ asset('js/jquery-1.12.4.min.js') }}"></script>
+    <script src="{{ secure_asset('js/jquery-1.12.4.min.js') }}"></script>
 </head>
 <body>
 <div>
     @yield('main')
 </div>
-<script src="{{ asset('js/app.js') }}" type="text/js"></script>
-<script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('magnificpopup/jquery.magnific-popup.min.js') }}"></script>
-<script src="{{ asset('js/wwb15.min.js') }}"></script>
-<script src="{{ asset('js/jscookmenu.min.js') }} "></script>
+<script src="{{ secure_asset('js/app.js') }}" type="text/js"></script>
+<script src="{{ secure_asset('js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ secure_asset('js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ secure_asset('magnificpopup/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ secure_asset('js/wwb15.min.js') }}"></script>
+<script src="{{ secure_asset('js/jscookmenu.min.js') }} "></script>
 </body>
 </html>
