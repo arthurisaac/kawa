6ab9cdbfb7803f25973830a84cd0044da506476c
Izz24 <r.thur.light@gmail.com>
2020-09-15 13:44:31 +0000

    Maincourante commit

--- app/Http/Controllers/CommercialSiteController.php
+++ app/Http/Controllers/CommercialSiteController.php
@@ -54,6 +54,10 @@ class CommercialSiteController extends Controller
      */
     public function store(Request $request)
     {
+        $objet_operation = null;
+        if (!empty($request->get('objet_operation'))) {
+            $contrat_regime = implode(",", $request->get('objet_operation'));
+        }
         $site = new Commercial_site([
             'client' => $request->get('client'),
             'site' => $request->get('site'),
@@ -63,7 +67,7 @@ class CommercialSiteController extends Controller
             'centre_regional' => $request->get('centre_regional'),
             'telephone' => $request->get('telephone'),
             'no_carte' => $request->get('no_carte'),
-            'objet_operation' => implode(",", $request->get('objet_operation')),
+            'objet_operation' => $contrat_regime,
             'forfait_mensuel_ctv' => $request->get('forfait_mensuel_ctv'),
             'forfait_mensuel_gdf' => $request->get('forfait_mensuel_gdf'),
             'forfait_mensuel_mad' => $request->get('forfait_mensuel_mad'),
