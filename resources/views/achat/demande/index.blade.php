7442243778d8bc264a10b71998409f01f002e5e5
Arthur Sondo <r.thur.light@gmail.com>
2021-09-15 14:21:37 +0000

    Corrections maincourante

--- resources/views/achat/demande/index.blade.php
+++ resources/views/achat/demande/index.blade.php
@@ -28,11 +28,11 @@
 
             <ul class="nav nav-tabs tabs-dark bg-dark" id="myTab" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link {{ str_contains(Session::get('user')->compte ?? '', 'operateur') ? '' : 'disabled' }}" id="entreprise-tab" data-toggle="tab" href="#demande" role="tab"
+                    <a class="nav-link {{ str_contains(Session::get('user')->compte ?? '', 'operateur') ? '' : '' }}" id="entreprise-tab" data-toggle="tab" href="#demande" role="tab"
                        aria-controls="demande" aria-selected="true">Informations</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ str_contains(Session::get('user')->compte ?? 'operateur', 'operateur') ? 'disabled' : '' }}" id="imputations-tab" data-toggle="tab" href="#imputations" role="tab"
+                    <a class="nav-link {{ str_contains(Session::get('user')->compte ?? 'operateur', 'operateur') ? '' : '' }}" id="imputations-tab" data-toggle="tab" href="#imputations" role="tab"
                        aria-controls="imputations" aria-selected="false">Imputation</a>
                 </li>
             </ul>
