9627d15cd89b8e6aa0262ebbb31fee54bff1c6e9
arthurisaac <r.light.light@gmail.com>
2021-10-22 13:47:52 +0000

    Regulation

--- resources/views/regulation/arrivee-tournee/index.blade.php
+++ resources/views/regulation/arrivee-tournee/index.blade.php
@@ -193,12 +193,12 @@
                             text: `${site} (${clients.client_nom})`
                         }));
                     });
-                    changeColis();
                 }
                 const departSites = sites.filter(v => {
                     return parseInt(v.idTourneeDepart) === parseInt(this.value);
                 });
                 if (departSites) populateSites(departSites);
+                changeColis();
             });
         });
 
@@ -384,19 +384,31 @@
             let index = 0;
             const thisColisInput = this;
             // Trouver l'index du champs actuel
-            $.each($("select[name='site[]']"), function (i) {
-                const site = $("select[name='site[]']").get(i);
-                if (thisColisInput === site) {
+            $.each($("select[name='colis[]']"), function (i) {
+                const colis = $("select[name='colis[]']").get(i);
+                if (thisColisInput === colis) {
                     index = i;
                 }
+                console.log(colis.value)
+                if (colis.value === "RAS" ) {
+                    $("input[name='valeur_colis_xof[]']").eq(i).prop('disabled', true);
+                    $("input[name='device_etrangere_dollar[]']").eq(i).prop('disabled', true);
+                    $("input[name='device_etrangere_euro[]']").eq(i).prop('disabled', true);
+                    $("input[name='pierre_precieuse[]']").eq(i).prop('disabled', true);
+                    $("textarea[name='numero[]']").eq(i).prop('disabled', true);
+                    $("input[name='nbre_colis[]']").eq(i).prop('disabled', true);
+                    $("select[name='nature[]']").eq(i).prop('disabled', true);
+                } else {
+                    $("input[name='valeur_colis_xof[]']").eq(i).prop('disabled', false);
+                    $("input[name='device_etrangere_dollar[]']").eq(i).prop('disabled', false);
+                    $("input[name='device_etrangere_euro[]']").eq(i).prop('disabled', false);
+                    $("input[name='pierre_precieuse[]']").eq(i).prop('disabled', false);
+                    $("textarea[name='numero[]']").eq(i).prop('disabled', false);
+                    $("input[name='nbre_colis[]']").eq(i).prop('disabled', false);
+                    $("select[name='nature[]']").eq(i).prop('disabled', false);
+                }
             });
-            $("input[name='valeur_colis_xof[]']").eq(index).prop('disabled', true);
-            $("input[name='device_etrangere_dollar[]']").eq(index).prop('disabled', true);
-            $("input[name='device_etrangere_euro[]']").eq(index).prop('disabled', true);
-            $("input[name='pierre_precieuse[]']").eq(index).prop('disabled', true);
-            $("textarea[name='numero[]']").eq(index).prop('disabled', true);
-            $("input[name='nbre_colis[]']").eq(index).prop('disabled', true);
-            $("select[name='nature[]']").eq(index).prop('disabled', true);
+
         }
     </script>
 @endsection
